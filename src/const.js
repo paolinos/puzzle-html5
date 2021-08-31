94eac4334d0b72fc416eb8b4ab9679f5b2c961ce
Pablo C <pendlejin@yahoo.com.ar>
2021-08-31 21:52:53 +0200

    add const file

---
+++ src/const.js
@@ -0,0 +1,24 @@
+// Game status
+export const PUZZLE_GAME_STATUS = {
+    NONE: 0,
+    LOADING_IMAGE: 10,
+    PRE_PREVIEW: 15,
+    PREVIEWING: 20,
+    PLAYING: 30
+}
+
+
+// Time game in seconds
+export const TIME_GAME = 60;
+export const TO_SECONDS = 0.001;
+
+export const PUZZLE_TABS = {
+    UP: "tab-up",
+    DOWN: "tab-down",
+    LEFT: "tab-left",
+    RIGHT: "tab-right"
+};
+
+// Validation Const
+export const VALIDATION_INPUT_MIN_VALUE = 3;
+export const VALIDATION_INPUT_FILE_LENGTH = 5;

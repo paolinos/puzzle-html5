c2d72a0aad0135e345b51e89648a76e2eeb55586
Pablo C <pendlejin@yahoo.com.ar>
2022-03-01 18:43:25 +0100

    Remove files that are not necessary or not used by the last Engine2d refactor

--- src/render/draganddrop.render.js
+++
@@ -1,8 +0,0 @@
-import BaseObjectRender from "./base.object.render";
-
-
-export default class DragAndDropRenderDispatcher extends BaseObjectRender {
-    constructor() {
-        super();
-    }
-}
